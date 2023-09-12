6bd1717cbd03a0f9d6a7f95db99b35a97feb8ef8
Erin <asf5947@g.rit.edu>
2023-09-12 19:30:38 -0400

    added potion manager stuff

---
+++ WitchPotions/Assets/PotionManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionManager : MonoBehaviour
+{
+    // int array PotionQualities {rage/terror, grief/joy,
+    // vigilance/amazement, loathing/admiration}
+    // goes from -25 to 25 on each
+
+    // int money
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    /// <summary>
+    /// Adds the quality of a given value (positive or negative) to the potion array
+    /// </summary>
+    /// <param name="qualityValue"></param>
+    /// <param name="qualityIndex"></param>
+    void AddQuality(int qualityValue, int qualityIndex)
+    {
+
+    }
+
+    /// <summary>
+    /// Finishes The potion, sending it to the game manager
+    /// then resets the potion stats
+    /// </summary>
+    void FinishPotion()
+    {
+
+    }
+
+    /// <summary>
+    /// Resets the currently brewed potion
+    /// (and all money that was spent on it)
+    /// </summary>
+    void ResetPotion()
+    {
+
+    }
+
+    
+
+}
