85b5f915eb22b75a3b3a0bc2bb2855413d93bce0
Erin <asf5947@g.rit.edu>
2023-09-26 20:46:45 -0400

    Fully functional node graph and path generation
    plus some comments to make things slightly sensical

--- WitchPotions/Assets/Scripts/potionBehavior.cs
+++ WitchPotions/Assets/Scripts/potionBehavior.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class potionBehavior : MonoBehaviour
 {
     Vector3 center;
-    List<Vector3> nodes;
+    List<Vector3> nodes = new List<Vector3>();
     private void Start()
     {
         center = gameObject.transform.localPosition;
 
+        //potion position init
         Vector3[] nodetemplate = new Vector3[] {Vector3.zero, Vector3.zero, Vector3.zero};
         nodetemplate[0] = center + new Vector3( 25, 0, 0 );
         nodetemplate[1] = center + new Vector3(45, 0, 0);
         nodetemplate[2] = center + new Vector3(60, 0, 0);
         nodes.Add(center);
         //nodes.AddRange(nodetemplate);
-        for (int i = 0; i > 8; i++)
+        //populate node graph
+        for (int i = 0; i < 8; i++)
         {
-            for (int j = 0; j > 3; j++)
+            for (int j = 0; j < 3; j++)
             {
                 Vector3 newNode;
                 newNode = Quaternion.Euler(0, 0, (i * 45)) * nodetemplate[j];
@@ -27,17 +29,20 @@ public class potionBehavior : MonoBehaviour
         }
     }
 
-    void moveTowardsFixed(string ingredientIndex)
+    //intakes an ingredient name and calls the moveToward function with the appropriate values
+   public void moveTowardsFixed(string ingredientIndex)
     {
         (Vector3, float) h = ingredientIndex switch
         {
-            "hi" => (new Vector3(60, 0, 0), 14f),
+            "reggie" => (new Vector3(60, 0, 0), 20f),
+            "fortnite" => (new Vector3(0, 60, 0), 20f),
             _ => (new Vector3(60, 0, 0), 14f)
         };
         moveToward(h);
         
     }
     
+    // moves toward a given endpoint by a certain distance
     void moveToward((Vector3, float) inputs) {
         (Vector3 node, float distance) = inputs;
         Vector3 v = (node - transform.localPosition).normalized * distance;
@@ -45,6 +50,8 @@ public class potionBehavior : MonoBehaviour
         //set final as destination node with distance 
         Vector3 finalNode = node;
         float distBest = Vector3.Distance(node,vSpace);
+
+        //calcualtes the closest node and locks the point's position to it
         foreach (Vector3 point in nodes)
         {
             float temp = Vector3.Distance(vSpace, point);
@@ -52,6 +59,7 @@ public class potionBehavior : MonoBehaviour
             if (distBest > temp) { distBest = temp; finalNode = point; }
         }
         transform.localPosition = finalNode;
+
     }
 }
 
