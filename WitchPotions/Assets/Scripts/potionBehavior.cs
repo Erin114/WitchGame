89745c82cc0bc069a0a9559e362a6a688d4a438d
Erin <asf5947@g.rit.edu>
2023-10-02 11:30:54 -0400

    adjusting potionbehavior to be more consistient
    tweaked values, changed the number of rings to 8

--- WitchPotions/Assets/Scripts/potionBehavior.cs
+++ WitchPotions/Assets/Scripts/potionBehavior.cs
@@ -6,21 +6,26 @@ public class potionBehavior : MonoBehaviour
 {
     Vector3 center;
     List<Vector3> nodes = new List<Vector3>();
+    float unit;
     private void Start()
     {
         center = gameObject.transform.localPosition;
 
-        //potion position init
-        Vector3[] nodetemplate = new Vector3[] {Vector3.zero, Vector3.zero, Vector3.zero};
-        nodetemplate[0] = center + new Vector3( 25, 0, 0 );
-        nodetemplate[1] = center + new Vector3(45, 0, 0);
-        nodetemplate[2] = center + new Vector3(60, 0, 0);
+        //potion position init, resolve how many rings will be in use and what the base ring units will be
+        int rings = 8;
+        Vector3[] nodetemplate = new Vector3[rings];
+        unit = 60f / rings;
+        for (int i = 0; i < rings; i++)
+        {
+            nodetemplate[i] = center + new Vector3(i * unit, 0, 0);
+        }
+        
         nodes.Add(center);
         //nodes.AddRange(nodetemplate);
         //populate node graph
         for (int i = 0; i < 8; i++)
         {
-            for (int j = 0; j < 3; j++)
+            for (int j = 0; j < rings; j++)
             {
                 Vector3 newNode;
                 newNode = Quaternion.Euler(0, 0, (i * 45)) * nodetemplate[j];
@@ -29,13 +34,18 @@ public class potionBehavior : MonoBehaviour
         }
     }
 
+
+
     //intakes an ingredient name and calls the moveToward function with the appropriate values
    public void moveTowardsFixed(string ingredientIndex)
     {
+        //ingredient switch case
         (Vector3, float) h = ingredientIndex switch
         {
-            "reggie" => (new Vector3(60, 0, 0), 20f),
-            "fortnite" => (new Vector3(0, 60, 0), 20f),
+            "reggie" => (new Vector3(60, 0, 0), unit * 2),
+            "joy" => (new Vector3(0, 60, 0), unit * 2),
+            "rage" => (new Vector3(-60, 0, 0), unit * 2),
+            "grief" => (new Vector3(0,-60,0), unit * 2),
             _ => (new Vector3(60, 0, 0), 14f)
         };
         moveToward(h);
@@ -51,15 +61,23 @@ public class potionBehavior : MonoBehaviour
         Vector3 finalNode = node;
         float distBest = Vector3.Distance(node,vSpace);
 
-        //calcualtes the closest node and locks the point's position to it
+        //calcualtes the closest node (that is NOT the current node) and locks the point's position to it
         foreach (Vector3 point in nodes)
         {
             float temp = Vector3.Distance(vSpace, point);
 
-            if (distBest > temp) { distBest = temp; finalNode = point; }
+            if (distBest > temp && point != transform.localPosition) { distBest = temp; finalNode = point; }
         }
         transform.localPosition = finalNode;
 
+    }
+
+    Vector3 CreatePathEndpoint(Vector3 node, float distance)
+    {
+        Vector3 v = (node - transform.localPosition).normalized * distance;
+        Vector3 vSpace = v + transform.localPosition;
+
+        return vSpace;
     }
 }
 
