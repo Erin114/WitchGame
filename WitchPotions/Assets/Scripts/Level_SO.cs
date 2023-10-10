6513d9e1147848d9df788413cd98f22cacabf344
Erin <asf5947@g.rit.edu>
2023-10-10 16:33:09 -0400

    fixed gitignore and redid references (elad is crying)

--- WitchPotions/Assets/Scripts/Level_SO.cs
+++ WitchPotions/Assets/Scripts/Level_SO.cs
@@ -26,6 +26,7 @@ public class Level_SO : ScriptableObject
     [SerializeField] NodeTypes[] nodeType;
     [SerializeField] int endpoint_Index;
     [SerializeField] int coin_Reward;
+    [SerializeField] int[] tags;
     public int Endpoint_Index { get => endpoint_Index; }
     public (int[] emotionIndex, NodeTypes[] type) Special_Nodes_List
     {
